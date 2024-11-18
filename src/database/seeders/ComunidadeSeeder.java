ec84ea7bf0281a9d0439706bd9a6d763f399e5f2
ArthurTakamori <arthurguilhermes@gmail.com>
2024-11-17 21:58:22 -0300

    feat: getUsuariosFicticios Method: associate fakeUsers' adresses to correspondent neighborhoods; getEnderecoUsuario Method: Adresses' hashmap to each user; Fake Users are now associated to community by setUsuarios method.

--- src/database/seeders/ComunidadeSeeder.java
+++ src/database/seeders/ComunidadeSeeder.java
@@ -3,7 +3,9 @@ package database.seeders;
 import database.interfaces.DatabaseSeederInterface;
 import domain.models.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ComunidadeSeeder implements DatabaseSeederInterface<Comunidade> {
@@ -24,16 +26,61 @@ public class ComunidadeSeeder implements DatabaseSeederInterface<Comunidade> {
 
         Comunidade comunidade = this.getComunidade(bairro);
 
+        // Users ficticios associados a comunidade
+        comunidade.setUsuarios(getUsuariosFicticios(bairro));
+
         comunidades.put(comunidade.getId(), comunidade);
-        
+
         return comunidades;
     }
 
     private Comunidade getComunidade(Bairro bairro) {
         return new Comunidade(
-            "Brasilândia",
-            "Comunidade de São Paulo",
-            bairro
+                "Brasilândia",
+                "Comunidade de São Paulo",
+                bairro
         );
     }
+
+    private List<Usuario> getUsuariosFicticios(Bairro bairro) {
+        List<Usuario> usuarios = new ArrayList<>();
+
+        Usuario usuario1 = new Usuario(
+                "João",
+                "Silva",
+                12345678901L,
+                "joao@email.com",
+                "senha123",
+                getEnderecoUsuario(bairro, "Rua A", "101")
+        );
+
+        Usuario usuario2 = new Usuario(
+                "Maria",
+                "Oliveira",
+                98765432100L,
+                "maria@email.com",
+                "senha123",
+                getEnderecoUsuario(bairro, "Rua B", "202")
+        );
+
+        usuarios.add(usuario1);
+        usuarios.add(usuario2);
+
+        return usuarios;
+    }
+
+    private HashMap<String, UsuarioEndereco> getEnderecoUsuario(Bairro bairro, String logradouro, String numero) {
+        HashMap<String, UsuarioEndereco> enderecos = new HashMap<>();
+
+        UsuarioEndereco endereco = new UsuarioEndereco(
+                bairro,
+                logradouro,
+                numero
+        );
+
+        enderecos.put(endereco.getId(), endereco);
+
+        return enderecos;
+    }
 }
+
